3c6282382501001316ce29d202de1ce46211c4a8
Jibin Mathews <jibinmathews7@gmail.com>
2016-09-09 23:39:45 +0530

    Word wrapping at end of lines

--- test/server.js
+++ test/server.js
@@ -1,40 +1,43 @@
-console.log("Hello world. Starting test");
+// console.log("Hello world. Starting test");
 
 
-var newLog = new require('..');
-// newLog.config({name: 'jibin'});
-// newLog.addGlobalConfig('defaultTag', "this is default tag");
+// var newLog = new require('..');
+// // newLog.config({name: 'jibin'});
+// // newLog.addGlobalConfig('defaultTag', "this is default tag");
 
-console.log("Start Time: "+(new Date).getTime());
-newLog.e( "This should be new error");
-newLog.w("Tag", "This should be new warn");
-newLog.i("Tag", "This should be new info");
-newLog.d("Tag", "This should be new debug");
+// console.log("Start Time: "+(new Date).getTime());
+// newLog.e( "This should be new error");
+// newLog.w("Tag", "This should be new warn");
+// newLog.i("Tag", "This should be new info");
+// newLog.d("Tag", "This should be new debug");
+
+// // Log.hr();
+
+// var Log = require('..');
+
+
+// // Log.config({'defaultTag': "nothing"});
+
+// Log.e("This is error");
+// Log.w("This is warning");
+// Log.i("This is info");
+// Log.d("This is debug");
 
 // Log.hr();
-
-var Log = require('..');
-
-
-// Log.config({'defaultTag': "nothing"});
-
-Log.e("This is error");
-Log.w("This is warning");
-Log.i("This is info");
-Log.d("This is debug");
-
-Log.hr();
 
 var Log2 = require('..');
 var Log2 = new Log2.Logger({'defaultTag': "Jibin", "hrChar": "*", "projectName": "MyTestAPp", "tagBold": true,"showProjectName":false});
 // Log2.config();
 
-Log2.e("This is error");
-Log2.w("This is warning");
-Log2.i("Lorem ipsum dolor sit amet, consectetur adipiscing elit. aaaaaaaaaa abcd sem x?? yz. bbbb nec consectetur risus, in finibus arcu. Suspendisse in placerat lacus, ac condimentum purus. Praesent vestibulum dapibus tempor. Nam ipsum diam, semper ut massa ac, congue iaculis nisi. Aenean sed dignissim sem, eget viverra nibh. Vivamus consectetur faucibus ante vitae molestie. Integer blandit convallis erat et facilisis. Aliquam turpis sem, euismo odio id semper ultrices, felis neque malesuada lacus, nec porta nunc nulla iaculis purus.");
-Log2.d("This is debug");
+// Log2.e("This is error");
+// Log2.w("This is warning");
+Log2.i("Node.js is an open-source, cross-platform JavaScript runtime environment for developing a diverse variety of tools and applications. Although Node.js is not a JavaScript framework,[3] many of its basic modules are written in JavaScript, and developers can write new modules in JavaScript. The runtime environment interprets JavaScript using Google's V8 JavaScript engine. "+
+  "\nNode.js has an event-driven architecture capable of asynchronous I/O. These design choices aim to optimize throughput and scalability in Web applications with many input/output operations, as well as for real-time Web applications (e.g., real-time communication programs and browser games).");
 
-Log2.d("blue", "", "This is debug");
+Log2.e("Android is a mobile operating system developed by Google, based on the Linux kernel and designed primarily for touchscreen mobile devices such as smartphones and tablets. Android's user interface is mainly based on direct manipulation, using touch gestures that loosely correspond to real-world actions, such as swiping, tapping and pinching, to manipulate on-screen objects, along with a virtual keyboard for text input. In addition to touchscreen devices, Google has further developed Android TV for televisions, Android Auto for cars, and Android Wear for wrist watches, each with a specialized user interface. Variants of Android are also used on notebooks, game consoles, digital cameras, and other electronics.");
+// Log2.d("This is debug");
+//
+// Log2.d("blue", "", "This is debug");
 
 
-Log2.hr(15, true, "green", "", "=");
+// Log2.hr(15, true, "green", "", "=");
